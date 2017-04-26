265e303e934ad6fb9c7bb1f2d20c9e1e8cf3af08
kyleyu <kyleyu.work@gmail.com>
2017-04-25 23:23:54 -0700

    GeoQuiz Chapter2 Challenge

--- app/src/main/java/com/bignerdranch/yi/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/yi/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,7 +12,8 @@ public class QuizActivity extends AppCompatActivity {
 
   private Button mTrueButton;
   private Button mFalseButton;
-  private Button mNextButton;
+  private ImageButton mPrevButton;
+  private ImageButton mNextButton;
   private TextView mQuestionTextView;
 
   private Question[] mQuestionBank = new Question[] {
@@ -31,6 +33,13 @@ public class QuizActivity extends AppCompatActivity {
     setContentView(R.layout.activity_quiz);
 
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+    mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        updateQuestion();
+      }
+    });
     updateQuestion();
 
     mTrueButton = (Button) findViewById(R.id.true_button);
@@ -49,7 +58,16 @@ public class QuizActivity extends AppCompatActivity {
       }
     });
 
-    mNextButton = (Button) findViewById(R.id.next_button);
+    mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+    mPrevButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mCurrentIndex = (mCurrentIndex + mQuestionBank.length - 1) % mQuestionBank.length;
+        updateQuestion();
+      }
+    });
+
+    mNextButton = (ImageButton) findViewById(R.id.next_button);
     mNextButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
